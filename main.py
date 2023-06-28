e7b06b66cfe5f1c429fbb5ac7cba6cd652eb8aae
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-06-28 12:27:37 +0200

    current course dollars

---
+++ main.py
@@ -0,0 +1,66 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_message(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_message('something')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+first_name = 'Iryna'
+second_name = 'Lititanskas'
+full_name = first_name + ' '+ second_name
+print ("Hello, " + full_name )
+
+my_name = 'iryna lititanskas'
+print(my_name.title())
+company_info = 'irina lititanskas'
+print(company_info.capitalize())
+
+first_name = '    Iryna    '
+second_name = '    Lititanskas    '
+print('\tIryna')
+print('\tLititanskas')
+print("hello my_name is " + full_name + '\nand i m here to learn python' )
+
+first_string = 'Iryna '
+second_string = ' Lititanskas'
+print(first_string.rstrip())
+print(second_string.lstrip())
+print(full_name.strip())
+
+
+x = 36.7
+y = 1000
+result = (y / x)
+print(round(result, 2))
+print(f'Поточний курс складає: {y} гривень, {round(result,2)} долларів")')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
